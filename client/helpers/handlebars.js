d017a04bc5d713367587e433cc2204fea4d8d43d
tanmay80 <tanmay.bhide@gmail.com>
2013-10-21 18:27:37 +0000

    first commit

---
+++ client/helpers/handlebars.js
@@ -0,0 +1,20 @@
+Handlebars.registerHelper('pluralize', function(n, thing) {
+  // fairly stupid pluralizer
+  if (n === 1) {
+    return '1 ' + thing;
+  } else {
+    return n + ' ' + thing + 's';
+  }
+});
+Handlebars.registerHelper('removeImgTag', function(text) {
+/*	
+	if(text.indexOf("<img") > 0) {
+		return text.split('<')[0];
+	}
+*/
+});
+
+sayHelloWorld = function() 
+{
+   return ('Hello Beautiful'); 
+}
